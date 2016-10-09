f3c9dcf590b9e5f0f1ac315e5bcbaa805e020e2c
barrybalondo <barry_balondo@hotmail.com>
2016-10-09 03:40:39 -0400

    Put seed in DB

--- config/index.js
+++ config/index.js
@@ -3,8 +3,7 @@ var configValues = require('./config');
 module.exports = {
 
     getDbConnectionString: function() {
-        //<dbuser>:<dbpassword>@ds053186.mlab.com:53186/nodetodobarry
-        return 'mongodb://' + configValues.uname + ':' + configValues.pass + '@ds053186.mlab.com:53186/nodetodobarry'
+        return 'mongodb://' + configValues.uname + ':' + configValues.pass + '@ds053186.mlab.com:53186/nodetodobarry';
     }
 
 }
